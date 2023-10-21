2b240b81728496680a2225ec28eadc5e5b71b82d
repetitioestmaterstudiorum <44611591+repetitioestmaterstudiorum@users.noreply.github.com>
2023-10-21 12:29:06 +0200

    release 0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pt_fc_layers_viz',
-    version='0.5',
+    version='0.6',
     packages=find_packages(),
     install_requires=[
         'graphviz',
